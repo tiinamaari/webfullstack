8fc965bed5d902efb903946b4b6ed643477d441f
Tiina Leppakoski <tiina.leppakoski@gmail.com>
2018-09-25 14:53:36 +0300

    testi committi

---
+++ osa1/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <p>Hello world jahut</p>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
